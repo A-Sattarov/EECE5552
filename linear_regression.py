5841411640add75f77c5314a1e10b8896b8d9c65
Abdusattor Sattarov PC <sattarov.a@northeastern.edu>
2022-12-08 12:10:41 -0500

    data categorization/xyz extraction function added to linear regression script

--- linear_regression.py
+++ linear_regression.py
@@ -1,12 +1,29 @@
 import numpy as np
-import h5py
-import pickle
-import io
 from feature_extraction import *
 import pandas as pd
 
-f = pd.read_pickle('FallAllD.pkl')
+pd.options.mode.chained_assignment = None  # default='warn'
 
-flat_frame, Act_ID = process_raw(f)
+'''
+SubjectID      - the number of the person from whom the data were collected (15 in total)
+Device         - from what type of device the data were collected from (Waist, Wrist, and Neck)
+ActivityID     - the ID of the activity that the person was conducting (135 in total)
+TrialNo        - the number of the trial that the activity was performed by the subject
+Acc            - Acceleration data
+Gyr            - Gyroscope data
+'''
 
-print(flat_frame)
+# This function gets properties and outputs x y z values of these properties as a dictionary
+# Acc=True means you want acceleration data. Acc=False means you want Gyro data
+def categorize_data(SubjectID, Device, ActivityID, TrialNo, Acc):
+    df = pd.read_pickle('FallAllD.pkl')
+    for i in range(len(df)):
+        df.Acc[i] = {'x': df.Acc[i][:, 0], 'y': df.Acc[i][:, 1], 'z': df.Acc[i][:, 2]}
+        df.Gyr[i] = {'x': df.Gyr[i][:, 0], 'y': df.Gyr[i][:, 1], 'z': df.Gyr[i][:, 2]}
+    f = df.groupby("SubjectID", group_keys=True)[['Device', 'ActivityID', 'TrialNo', 'Acc', 'Gyr']].apply(lambda x: x.set_index(['Device', 'ActivityID', 'TrialNo']))
+    df = f.loc[(f.index.get_level_values('SubjectID') == SubjectID) & (f.index.get_level_values('Device') == Device) & (f.index.get_level_values('ActivityID') == ActivityID) & (f.index.get_level_values('TrialNo') == TrialNo)]
+    return df.reset_index().Acc[0] if Acc == True else df.reset_index().Gyr[0]
+
+
+data = categorize_data(SubjectID=1, Device='Waist', ActivityID=13, TrialNo=1, Acc=True)
+print(data)
